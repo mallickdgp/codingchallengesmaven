51e5908a3695b35aedce35eba6f32cc1466dd3ba
mallickdgp@gmail.com <mallickdgp@gmail.com>
2021-08-01 23:14:38 +0530

    hackerank practise challenges

--- src/main/java/com/mallickdgp/KangarooJumps.java
+++ src/main/java/com/mallickdgp/KangarooJumps.java
@@ -2,23 +2,29 @@ package com.mallickdgp;
 
 public class KangarooJumps {
 
-    public static void solution(int x1, int v1, int x2, int v2){
-        if(x1==x2 && v1 == v2)
-            System.out.println("YES");
-        else if(x1 != x2 && v1 == v2)
-            System.out.println("NO");
-        else{
-            int currentPoint1 = x1;
-            int currentPoint2 = x2;
-            while(true){
-                currentPoint1 = currentPoint1 + v1;
-                currentPoint2 = currentPoint2 + v2;
-                if(currentPoint1 == currentPoint2){
-                    System.out.println("YES");
-                    break;
-                }
+    /*
+    You are choreographing a circus show with various animals. For one act, you are given two kangaroos
+    on a number line ready to jump in the positive direction (i.e, toward positive infinity)
+The first kangaroo starts at location x1and moves at a rate of v1meters per jump
+The second kangaroo starts at location x2and moves at a rate of v2meters per jump
+You have to figure out a way to get both kangaroos at the same location at the same time as part of the show.
+If it is possible, return YES. Otherwise, return NO.
+For example, kangaroo 1 starts at x1 = 2 with a jump distance of v1 = 1and kangaroo 2 start at x2= 1with a jump
+ distance of v2 = 2. After one jump, they are both at x = 3, as x1 + v1 = 2+1 = 3, x2 + v2 = 1+2 = 3,
+ so our answer is YES
+     */
 
-            }
-        }
+    public static String solution(int x1, int v1, int x2, int v2){
+        if(x1 == x2 && v1 != v2)
+            return "NO";
+        if(x1 < x2 && v1 < v2)
+            return "NO";
+        else if(x1 > x2 && v1 > v2)
+            return  "NO";
+        if ((v1 != v2) && ((x1 - x2) % (v2 - v1) == 0 || (x2 - x1) % (v1 - v2) == 0))
+                return "YES";
+
+        else
+            return "NO";
     }
 }
--- src/test/java/com/mallickdgp/KangarooJumpsTest.java
+++ src/test/java/com/mallickdgp/KangarooJumpsTest.java
@@ -2,8 +2,46 @@ package com.mallickdgp;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class KangarooJumpsTest {
 
     @Test
-    public void testWithSameStarting(){}
+    public void testWithSameStarting(){
+        int x1=0;
+        int x2=0;
+
+        int v1=1;
+        int v2=2;
+
+        String ans = KangarooJumps.solution(x1, v1, x2, v2);
+
+        assertEquals("NO", ans);
+    }
+
+    @Test
+    public void testWithX1grtrthnX2andV1lessthnV2(){
+        int x1=5;
+        int x2=3;
+
+        int v1=2;
+        int v2=3;
+
+        String ans = KangarooJumps.solution(x1, v1, x2, v2);
+
+        assertEquals("YES", ans);
+    }
+
+    @Test
+    public void TC01UC03(){
+        int x1=0;
+        int x2=5;
+
+        int v1=2;
+        int v2=3;
+
+        String ans = KangarooJumps.solution(x1, v1, x2, v2);
+
+        assertEquals("NO", ans);
+    }
 }
